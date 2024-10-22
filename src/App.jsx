d18d689d4629e74b855d0ed1777ace57c8207595
Jari Tenhunen <jari.tenhunen@outlook.com>
2024-10-22 14:19:08 +0200

    Can add things to list

--- src/App.jsx
+++ src/App.jsx
@@ -7,35 +7,66 @@ function App() {
   const [eatable, setEatable] = useState(true);
   const [id, setId] = useState("");
 
-  useEffect(() => {
-    async function fetchData() {
-      const result = await fetch("http://localhost:8080/list");
+  const fetchData = async () => {
+    const result = await fetch("http://localhost:8080/list");
       const body = await result.json();
       setItem(body);
       console.log("test");
-    }
+  }
+
+  useEffect(() => {
+    //async function fetchData() {
+      //const result = await fetch("http://localhost:8080/list");
+      //const body = await result.json();
+      //setItem(body);
+      //console.log("test");
+    //}
     fetchData();
   }, []);
 
-  
-    const listItems = item.map((item) => {
-      return (
-        <li key={item.id}>
-          <span>{item.name}</span>
-          <span>{item.eatable}</span>
-          <button>Remove</button>
-        </li>
-      )
+  const listItems = item.map((item) => {
+    return (
+      <li key={item.id}>
+        <span>{item.name}</span>
+        <span>{item.eatable}</span>
+        <button>Remove</button>
+      </li>
+    );
+  });
+
+  const handleClick = (event) => {
+    fetch('http://localhost:8080/list/add', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: name,
+        eatable: eatable,
+      })  
     })
+    fetchData()
+    setName("")
+    setEatable(false)
+  }
+
 
 
   return (
     <>
       <h1>Good Thing List</h1>
       <br />
-      <ul>
-        {listItems}
-        </ul>
+      <br />
+      <ul>{listItems}</ul>
+      <br />
+      <form>
+        <label for="AddThing">Add a Good Thing</label>
+        <input type="text" id="AddThing" value={name} onChange={() => setName(event.target.value)}/>
+        <label for="Eatable">Eatable</label>
+        <input type="checkbox" id="Eatable" value={eatable} onChange={() => setEatable(event.target.value)} />
+        <button type="button" onClick={handleClick}>Submit</button>
+      </form>
     </>
   );
 }
