acf9f0eba0d490a0ab57ebdf10dce44535cecfa2
Jari Tenhunen <jari.tenhunen@outlook.com>
2024-10-22 11:10:26 +0200

    Fetching seed data and displying list frame

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,43 @@
-
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [item, setItem] = useState([]);
+  const [name, setName] = useState("");
+  const [eatable, setEatable] = useState(true);
+  const [id, setId] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      const result = await fetch("http://localhost:8080/list");
+      const body = await result.json();
+      setItem(body);
+      console.log("test");
+    }
+    fetchData();
+  }, []);
+
+  
+    const listItems = item.map((item) => {
+      return (
+        <li key={item.id}>
+          <span>{item.name}</span>
+          <span>{item.eatable}</span>
+          <button>Remove</button>
+        </li>
+      )
+    })
+
+
   return (
     <>
-      <h1>Hello</h1>
+      <h1>Good Thing List</h1>
+      <br />
+      <ul>
+        {listItems}
+        </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
